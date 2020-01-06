b8d23273ff36b2ab69898f3ae4f8aef1f6b3f294
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-01-06 16:25:31 +0000

    add more items

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React from "react";
 
-export default function Homepage () {
-    return(
-        <div>
-            <div className="outer-box">
-                <div className="inner-box">
-                    <h2>HAT</h2>
-                    <span>Shop now</span>
-                </div>
-            </div>
+export default function Homepage() {
+  return (
+    <div>
+      <div>
+        <div className="outer-box">
+          <div className="inner-box">
+            <h2 className="title">HATS</h2>
+            <span className="subtitle">Shop now</span>
+          </div>
         </div>
-    );
+        <div className="outer-box">
+          <div className="inner-box">
+            <h2 className="title">SHOES</h2>
+            <span className="subtitle">Shop now</span>
+          </div>
+        </div>
+        <div className="outer-box">
+          <div className="inner-box">
+            <h2 className="title">BAGS</h2>
+            <span className="subtitle">Shop now</span>
+          </div>
+        </div>
+        <div className="outer-box">
+          <div className="inner-box">
+            <h2 className="title">DRINKS</h2>
+            <span className="subtitle">Shop now</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
