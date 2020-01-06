2f6284b49170fca1b0346efcb99e1c34131e1be6
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-01-06 15:56:11 +0000

    create homepage component

---
+++ client/src/components/Homepage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Homepage () {
+    return(
+        <div>
+            <div className="outer-box">
+                <div className="inner-box">
+                    <h2>HAT</h2>
+                    <span>Shop now</span>
+                </div>
+            </div>
+        </div>
+    );
+}
