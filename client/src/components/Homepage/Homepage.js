da32502d0c59c5bc35656ceca44d982f51aa409a
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-01-06 16:33:41 +0000

    add styles to homepage

--- client/src/components/Homepage/Homepage.js
+++ client/src/components/Homepage/Homepage.js
@@ -1,29 +1,30 @@
 import React from "react";
+import './styles.scss'
 
 export default function Homepage() {
   return (
     <div>
-      <div>
-        <div className="outer-box">
-          <div className="inner-box">
+      <div className="outer-box">
+        <div className="inner-box">
+          <div className="content">
             <h2 className="title">HATS</h2>
             <span className="subtitle">Shop now</span>
           </div>
         </div>
-        <div className="outer-box">
-          <div className="inner-box">
+        <div className="inner-box">
+          <div className="content">
             <h2 className="title">SHOES</h2>
             <span className="subtitle">Shop now</span>
           </div>
         </div>
-        <div className="outer-box">
-          <div className="inner-box">
+        <div className="inner-box">
+          <div className="content">
             <h2 className="title">BAGS</h2>
             <span className="subtitle">Shop now</span>
           </div>
         </div>
-        <div className="outer-box">
-          <div className="inner-box">
+        <div className="inner-box">
+          <div className="content">
             <h2 className="title">DRINKS</h2>
             <span className="subtitle">Shop now</span>
           </div>
