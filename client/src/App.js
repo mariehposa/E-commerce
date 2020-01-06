b86f83409a9543155f74534b219467f6142ce72b
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-01-06 15:57:48 +0000

    render homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css'
+import './App.css';
+import Homepage from './components/Homepage';
 
 function App() {
   return (
     <div>
-      
+      <Homepage />
     </div>
   );
 }
