af16751b7f7dce85c2699160510b2c19ae272de9
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-01-10 11:36:20 +0000

    add styles

--- client/src/components/menuItem/menuCard.js
+++ client/src/components/menuItem/menuCard.js
@@ -3,7 +3,7 @@ import "./styles.scss";
 
 export default function MenuCard({ items }) {
   return (
-    <div className="outer-box">
+    <div className={`${items.size} outer-box`} style={{background: `url(${items.imageUrl})`}}>
       <div className="inner-box">
         <div className="content">
           <h2 className="title">{items.title}</h2>
