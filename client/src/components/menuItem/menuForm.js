a577e15db0bd6b421930f7900ef401d1d2de570f
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-12-19 17:19:45 +0100

    wip

--- client/src/components/menuItem/menuForm.js
+++ client/src/components/menuItem/menuForm.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 
 class MenuForm extends Component () {
+    constructor (){
+        super();
+        this.state = {
+
+        }
+    }
+    
     render () {
         return (
             <div>
-                <input type="text" placeholder="Name" />
                 <input type="text" placeholder="Username" />
+                <input type="email" placeholder="Email" />
                 <input type="text" placeholder="Password" />
                 <submit>Submit</submit>
             </div>
