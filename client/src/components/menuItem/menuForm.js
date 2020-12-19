701079adbc90bff3deeb7a55f5462c195ac9d6c2
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-12-19 17:14:47 +0100

    create class component for menuform

---
+++ client/src/components/menuItem/menuForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class MenuForm extends Component () {
+    render () {
+        return (
+            <div>
+    
+            </div>
+        );
+    }
+} 
